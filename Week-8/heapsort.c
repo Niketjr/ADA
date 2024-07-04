49cbeade7e5f36b3dd457d67feb344aa8d548da1
Niket Dugar <143904117+Niketjr@users.noreply.github.com>
2024-07-04 10:26:30 +0530

    Create heapsort.c

---
+++ Week-8/heapsort.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <time.h>
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Function to heapify a subtree rooted at index i in arr[]
+void heapify(int arr[], int n, int i) {
+    int largest = i;  // Initialize largest as root
+    int l = 2 * i + 1;  // left = 2*i + 1
+    int r = 2 * i + 2;  // right = 2*i + 2
+
+    // If left child is larger than root
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    // If right child is larger than largest so far
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    // If largest is not root
+    if (largest != i) {
+        swap(&arr[i], &arr[largest]);
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+// Main function to do heap sort
+void heapSort(int arr[], int n) {
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // One by one extract an element from heap
+    for (int i = n - 1; i > 0; i--) {
+        // Move current root to end
+        swap(&arr[0], &arr[i]);
+
+        // call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+    int a[15000], n, i, j, ch, temp;
+    clock_t start, end;
+
+    while (1) {
+        printf("\n1: For manual entry of N value and array elements");
+        printf("\n2: To display time taken for sorting number of elements N in the range 500 to 14500");
+        printf("\n3: To exit");
+        printf("\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch (ch) {
+            case 1:
+                printf("\nEnter the number of elements: ");
+                scanf("%d", &n);
+                printf("\nEnter array elements: ");
+                for (i = 0; i < n; i++) {
+                    scanf("%d", &a[i]);
+                }
+                start = clock();
+                heapSort(a, n);
+                end = clock();
+                printf("\nSorted array is: ");
+                for (i = 0; i < n; i++)
+                    printf("%d\t", a[i]);
+                printf("\n Time taken to sort %d numbers is %f Secs", n, (((double)(end - start)) / CLOCKS_PER_SEC));
+                break;
+            case 2:
+                n = 500;
+                while (n <= 14500) {
+                    for (i = 0; i < n; i++) {
+                        a[i] = n - i;
+                    }
+                    start = clock();
+                    heapSort(a, n);
+                    for (j = 0; j < 500000000; j++) { temp = 38 / 600; }
+                    end = clock();
+                    printf("\n Time taken to sort %d numbers is %f Secs", n, (((double)(end - start)) / CLOCKS_PER_SEC));
+                    n = n + 1000;
+                }
+                break;
+            case 3:
+                exit(0);
+        }
+        getchar();
+    }
+}

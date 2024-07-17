e087d505c6bfb02f7f1f1231cfe5a59eaa6a893e
Niket Dugar <143904117+Niketjr@users.noreply.github.com>
2024-07-18 00:34:35 +0530

    Update DFS.c

--- Week-4/DFS.c
+++ Week-4/DFS.c
@@ -1,61 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
 
-void DFS(int u, int n, int **a, int *s, int *res, int *j) {
-    s[u] = 1;
-    for (int v = 0; v < n; v++) {
-        if (a[u][v] == 1 && s[v] == 0) {
-            DFS(v, n, a, s, res, j);
+int vis[20];
+int res[20];
+int j;
+
+void dfs(int u,int n,int a[n][n]){
+    vis[u]=1;
+    for(int i=0;i<n;i++){
+        if(a[u][i]==1 && vis[i]==0){
+            dfs(i,n,a);
         }
     }
-    res[(*j)++] = u;
+    res[j++]=u;
+
 }
 
-void topologicalOrder(int n, int **a) {
-    int s[n];
-    int res[n];
-    int j = 0;
-
-    for (int i = 0; i < n; i++) {
-        s[i] = 0;
-    }
-
-    for (int u = 0; u < n; u++) {
-        if (s[u] == 0) {
-            DFS(u, n, a, s, res, &j);
-        }
-    }
-
-    printf("Topological Order: ");
-    for (int i = n - 1; i >= 0; i--) {
-        printf("%d ", res[i]);
-    }
-    printf("\n");
-}
-
-int main() {
+int main(){
     int n;
     printf("Enter the number of vertices: ");
     scanf("%d", &n);
-
-    int **a = (int **)malloc(n * sizeof(int *));
-    for (int i = 0; i < n; i++) {
-        a[i] = (int *)malloc(n * sizeof(int));
-    }
-
-    printf("Enter the adjacency matrix:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    int a[n][n];
+    printf("Enter adjacency matrix: ");
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
             scanf("%d", &a[i][j]);
         }
     }
 
-    topologicalOrder(n, a);
-
-    for (int i = 0; i < n; i++) {
-        free(a[i]);
+    for(int i=0;i<n;i++){
+        vis[i]=0;
     }
-    free(a);
+    j=0;
 
-    return 0;
+    for(int i=0;i<n;i++){
+        if(vis[i]==0){
+            dfs(i,n,a);
+        }
+    }
+
+    for(int i=n-1;i>=0;i--){
+        printf("%d ",res[i]);
+    }
 }
